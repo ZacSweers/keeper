307065c06070c08b081d1d5ae5c0f59101615bf7
Zac Sweers <pandanomic@gmail.com>
2020-05-22 00:11:16 -0400

    Remove demo code

--- keeper-gradle-plugin/src/main/kotlin/com/slack/keeper/VariantClasspathJar.kt
+++ keeper-gradle-plugin/src/main/kotlin/com/slack/keeper/VariantClasspathJar.kt
@@ -65,11 +65,6 @@ abstract class VariantClasspathJar : DefaultTask() {
   abstract val classpath: ConfigurableFileCollection
 
   fun from(vararg paths: Any) {
-    // This is magic
-    IntRange(0, 4).forEach {
-      if (it == 1) return@forEach
-      println("Hello everyone$it!")
-    }
     classpath.from(*paths)
   }
 
